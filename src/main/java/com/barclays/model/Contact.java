53b9070f8a62a601d9e9a5cdbd4c9ddf4d14eb77
Grace <gahogan2000@gmail.com>
2023-11-23 14:14:36 +0000

    Package structure

---
+++ src/main/java/com/barclays/model/Contact.java
@@ -0,0 +1,10 @@
+package com.barclays.model;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.NoArgsConstructor;
+
+@MappedSuperclass
+@NoArgsConstructor
+public abstract class Contact {
+
+}
---
+++ src/main/java/com/barclays/model/PersonalContact.java
@@ -0,0 +1,4 @@
+package com.barclays.model;
+
+public class PersonalContact {
+}
